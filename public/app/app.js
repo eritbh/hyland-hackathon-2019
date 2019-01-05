76e1358997b08e2fa55e8e6ff7064209e0191762
George <georgej1088@gmail.com>
2019-01-04 21:41:22 -0500

    Start doing the things

--- public/app/app.js
+++ public/app/app.js
@@ -1,15 +1,38 @@
 /* global Vue */
 'use strict';
 
-Vue.component('some-component', {
+Vue.component('gists-list', {
 	props: {
-		text: String
+		username: String,
+	},
+	data () {
+		return {
+			gists: [],
+		};
+	},
+	methods: {
+		name (gist) {
+			// Return filename of the first file (TODO)
+			return Object.keys(gist.files)[0];
+		}
 	},
 	template: `
-		<div class="some-component">
-			{{text}}
+		<div class="gists-list">
+			<div
+				v-for="gist in gists"
+				:key="gist.id"
+				class="gist-item"
+			>
+				{{name(gist)}}
+			</div>
 		</div>
 	`,
+	mounted () {
+		// TODO: this request is incomplete and nothing will load
+		fetch('https://api.github.com/users/Geo1088/gists').then(res => res.json).then(data => {
+			this.gists = data; // TODO
+		})
+	},
 });
 
 // Create the main Vue instance
